3adb8f1eea56126ecdb3f2197d696e6b33d6937f
Urvi-patel27 <urvikathrotiyal@gmail.com>
2025-07-15 17:09:33 +0530

    u

---
+++ backend/config/mongodb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    try {
+        //connection
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("✔️ Database Connected")
+    } catch (error) {
+        console.error("❌ Database connection failed:", error.message)
+    }
+}
+export default connectDB
